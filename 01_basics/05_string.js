ee2725054b3331953a0c023559c62ebf855fcb6e
Vivek Badan <vivek.badan@gmail.com>
2024-07-16 16:35:56 +0000

    01_basics/05_string.js

---
+++ 01_basics/05_string.js
@@ -0,0 +1,10 @@
+const name="vivek";
+const repoCount=50;
+
+console.log(`hello my name is ${name} and my repoCount is  ${repoCount}`);
+let gameName="heloo darling !!!!"
+console.log(gameName[3]);
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(gameName.indexOf("g"));
