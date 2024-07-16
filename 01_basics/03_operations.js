d539925039ef9b31c5d488284ab45e74e4d52247
Vivek Badan <vivek.badan@gmail.com>
2024-07-16 10:37:19 +0000

    03 operations.js

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,50 @@
+let value=3;
+let negvalue = -value;
+console.log(negvalue);
+console.log (2+2);
+
+let str1 = "HELLO";
+let str2 = "VIVEK";
+
+console.log(str1+str2);
+console.log("1"+2+2);
+console.log(1+2+"3");
+
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+let num1 , num2 , num3;
+
+//dont do this ;
+
+let gamecounter = 100;
+gamecounter++;
+console.log(gamecounter);  
+
+
+
+
+//******************comparison datatype*****************
+
+console.log(3<9);
+console.log(2>3);
+console.log(2==1);
+console.log(1 !=2);
+
+
+//prblm occured n the whne the datatype is different i.e 1 + " string" 
+console.log(null>0);
+console.log(null>=0);
+console.log(null==0);
+console.log(null<0);
+
+// the reason is that an equaltiy check == and comaprison <>= works differently
+//Compaarions convert null number to a number, treating it as 0.
+
+
+// strict check ===
+
+console.log("2"===2);
