411cfb3ae990f47c794d2056473ecace1efcbf71
Vivek Badan <vivek.badan@gmail.com>
2024-07-16 10:05:41 +0000

    conversion+datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,27 @@
+console.log(typeof "hitesh");
+
+let AddressName="VivekBadan";
+let AddressId="22dec028";
+let AddressAge=21;
+
+console.table([AddressName,AddressId,AddressAge]);
+// conversion topic
+
+let score= 33;
+console.log(typeof score);
+console.log(typeof (score));
+
+let value=true;
+console.log(typeof value);
+let isloggedin =1;
+let booleanIsLoggedIn=Boolean(isloggedin);
+console.log(booleanIsLoggedIn);
+// 1 => true ; 0 => false 
+// " "=>false;
+//"hitesh"=> true;
+
+let somenumber=245;
+
+let change= String(somenumber);
+console.log(typeof somenumber);
+console.log(typeof change);
