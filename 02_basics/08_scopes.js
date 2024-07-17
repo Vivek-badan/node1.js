c3122557a29ca7e151855d108e3ae03931509d82
Vivek Badan <vivek.badan@gmail.com>
2024-07-17 09:55:15 +0000

    02_basics/08_scopes.js

---
+++ 02_basics/08_scopes.js
@@ -0,0 +1,14 @@
+let a= 10;
+const b=20;
+var c=30;
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+//   { } it is the scope of js , when it comes with the function it is called scope .
+
+// in other words ...when we put something into the {} it is local scope and when we put this outside it is global scope.
+
+ 
