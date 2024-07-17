cf0bd21cffcae67b00cf71300e1dd4c8d144d333
Vivek Badan <vivek.badan@gmail.com>
2024-07-17 09:19:54 +0000

    02_basics/05_obejcts3.js

---
+++ 02_basics/05_obejcts3.js
@@ -0,0 +1,34 @@
+const course ={
+    courseName:"js in hindi",
+    price:"999",
+    courseInstructor:"hitesh sir"
+
+}
+
+//course.courseInstructor
+
+const {courseInstructor:instructor}= course;
+//console.log(courseInstructor)
+console.log(instructor);
+
+//deconstructors : {  }
+
+//API :values from backend ;
+
+{
+    name: "vivek" ;
+    corseName: "js in eng";
+    price: "free"
+
+
+}
+
+
+
+[
+    {},
+    {},
+    {},
+    {},
+    
+]
