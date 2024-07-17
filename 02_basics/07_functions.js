91b743581da9e952f1aae5df260a8f08a886425d
Vivek Badan <vivek.badan@gmail.com>
2024-07-17 09:48:31 +0000

    02_basics/07_functions.js

---
+++ 02_basics/07_functions.js
@@ -0,0 +1,16 @@
+function calculationcarprice(...num1){
+    return num1;
+
+}
+console.log (calculationcarprice(200,300,400));
+
+const user ={
+    userName:"vivek",
+    price:456
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.userName} and price is ${anyobject.price}`);
+}
+
+handleObject(user);
