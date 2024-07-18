9f21eb30f55b9a1080c89af2f9506138e653cb42
Vivek Badan <vivek.badan@gmail.com>
2024-07-18 06:07:13 +0000

    03_basics/02_call-stack.js

---
+++ 03_basics/02_call-stack.js
@@ -0,0 +1,19 @@
+// javascript exectution content
+
+//global exectution content: it runs with THIS
+
+// function exectutuion content
+
+//eval executuion content
+
+// memory creation phase : sarra variables ko ek sath liya jata ha then rkha jata ha 
+
+// exectution phase 
+
+// step 1 : global exectution
+// step 2 : memory phase
+// step 3 : exectution phase
+
+
+
+
