455deb1a0a1c3d46d059337ecc979c50c81f7350
Vivek Badan <vivek.badan@gmail.com>
2024-07-19 07:34:14 +0000

    04_basics/05_loop.js

---
+++ 04_basics/05_loop.js
@@ -0,0 +1,34 @@
+const coding=['js','cpp','rb','py'];
+
+// coding.forEach( function (item){
+
+//     console.log(item);
+
+// })
+
+
+// coding.forEach((item)=>{
+//     console.log(item);
+
+// })
+
+// function printme(item){
+//     console.log(item);
+// }
+// coding.forEach(printme);
+
+
+
+const mycodinng=[
+    {
+    languagename:"js",
+    languagefilename:"javascript"
+
+},{
+    languagename:"py",
+    languagefilename:"python" 
+},]
+
+mycodinng.forEach((item)=>{
+    console.log(item.languagename);
+})
