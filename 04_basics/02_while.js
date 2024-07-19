dda1b3dbb608cff37f42043f671935983616048a
Vivek Badan <vivek.badan@gmail.com>
2024-07-19 06:54:58 +0000

    04_basics/02_while.js

---
+++ 04_basics/02_while.js
@@ -0,0 +1,24 @@
+// let index=0;
+
+// while(index<=10){
+
+//     console.log(`value of index is ${index}`)
+//     index +=2;
+// }
+
+// let myarr=['superman','batman','flash'];
+
+// let arr=0;
+// while(arr<myarr.length){
+//     console.log( `Value is ${myarr[arr]}`);
+//     arr++;
+// }
+
+
+let score =1;
+
+do {
+  console.log(`score is ${score}`);
+  score++;
+}
+while (score<=10)
