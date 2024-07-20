5cdd40116fe23efc4693cf71445727b4befa52bc
Vivek Badan <vivek.badan@gmail.com>
2024-07-20 06:55:22 +0000

    05_basics/01_dom.js

---
+++ 05_basics/01_dom.js
@@ -0,0 +1,14 @@
+<html>
+<head>
+    <title>
+        dom learning
+        </title>
+</head>
+<body>
+ <div class ='bg-black'>
+    <h1 class='heading'>
+        dom learning on chai aur code.
+    </h1>
+ </div>
+</body>
+</html>
